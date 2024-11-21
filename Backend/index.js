a6e064190a2a4f3b7115976bfb078850d60fc859
Rampandiyar <rampandiyar2@gmail.com>
2024-11-21 22:25:26 +0530

    json

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.json('Hello, World!');
 });
 app.listen(5000, ()=>{
     console.log('Server is running on port 5000');
